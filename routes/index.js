7b01fd537c99862c94861ef6ec1444506fa63203
Oluwaseyi <oluwaseyi.makinde@decagon.dev>
2026-02-07 22:37:39 +0100

    fixed logout

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,12 @@ routes.use("/employees", require("./employees"));
 routes.use("/inventory", require("./inventory"));
 routes.get(
   "/login",
-  passport.authenticate("github", (res, req) => {}),
+  passport.authenticate("github", (req, res) => {}),
 );
-routes.get("/logout", function (res, req, next) {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
+routes.get('/logout', (req, res, next) => {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
   });
 });
 module.exports = routes;
